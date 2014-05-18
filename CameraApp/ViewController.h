72df7e8e05298ce51faf4266b21ec6ca97b350fa
nishikawa <nishikawa@gu3.co.jp>
2014-05-19 01:40:52 +0900

    Initial Commit

---
+++ CameraApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraApp
+//
+//  Created by nishikawa kenichi on 2014/05/19.
+//  Copyright (c) 2014年 Nishikawa Kenichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
