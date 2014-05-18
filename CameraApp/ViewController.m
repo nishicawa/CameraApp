18dbb19fdf00ecb11a89b8b053c09f3cfbee4998
nishikawa <nishikawa@gu3.co.jp>
2014-05-19 02:27:09 +0900

    まだwarningは出ているが一応動くようになった。

--- CameraApp/ViewController.m
+++ CameraApp/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationBarDelegate>
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -25,5 +26,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)showImagePicker:(id)sender {
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:sourceType]) {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.sourceType = sourceType;
+        picker.delegate = self;
+        [self presentViewController:picker animated:YES completion:NULL];
+    }
+}
 
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *iamge = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:^{
+        self.imageView.image = iamge;
+        UIImageWriteToSavedPhotosAlbum(iamge, nil, nil, nil);
+    }];
+}
 @end
